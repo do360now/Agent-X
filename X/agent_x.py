b339a690883398feafb97ee3ccfc6aefb7dfc00a
Clement <cm@do360now.com>
2025-01-02 20:19:32 +0100

    fix tests

--- X/agent_x.py
+++ X/agent_x.py
@@ -3,9 +3,12 @@ import logging
 import time
 import random
 from authenticate import authenticate_v2, authenticate_v1
-from generate import find_ai_generated_image, generate_post_topic, gpt_generate_tweet
+from generate import (
+    find_ai_generated_image,
+    generate_post_topic,
+    gpt_generate_tweet,
+)
 from generate_image import generate_image
-
 
 # Configure logger
 logging.basicConfig(level=logging.INFO)
@@ -17,7 +20,6 @@ def main():
     logger.info("Starting authentication for Twitter API v2...")
     client = authenticate_v2()
     api_v1 = authenticate_v1() if client else None
-
 
     # Temporarlily disable the posting loop
     # Configurable frequency for posting (random between 1 to 2 hours)
@@ -33,7 +35,7 @@ def main():
         post_tweet(topic, client, api_v1)
 
         # Wait for the configured interval before posting the next tweet
-        logger.info(f"Waiting for {POST_INTERVAL} seconds before the next tweet...")
+        logger.info(f"Waiting for {POST_INTERVAL} seconds before next post.")
         time.sleep(POST_INTERVAL)
 
 
@@ -49,14 +51,14 @@ def post_tweet(topic, client, api_v1):
         if image_path:
             logger.info(f"Attempting to upload image at {image_path}...")
             media = api_v1.media_upload(filename=image_path)
-            logger.info(f"Image uploaded successfully: media_id = {media.media_id}")
+            logger.info(f"Image uploaded!: media_id = {media.media_id}")
             response = client.create_tweet(
                 text=tweet_content, media_ids=[media.media_id]
             )
-            logger.info(f"Tweeted successfully with image: {response.data['id']}")
+            logger.info(f"Posted with image: {response.data['id']}")
         else:
             response = client.create_tweet(text=tweet_content)
-            logger.info(f"Tweeted successfully without image: {response.data['id']}")
+            logger.info(f"Posted with no image: {response.data['id']}")
     except tweepy.TweepyException as e:
         logger.error(f"Failed to post tweet: {e}")
 
